05d8ab4853ca270d90b72d7e6f8e211dc19615eb
Kees Klerk <kees@finalist.com>
2013-02-10 17:20:48 +0100

    Remove wip.

--- features/movie.feature
+++ features/movie.feature
@@ -1,6 +1,6 @@
 Feature: Adding a movie
 
-  @wip
-  Scenario: Uploading a screenshot 
+  @javascript
+  Scenario: Uploading a screenshot
     When I make a new movie entry and upload a screenshot
     Then I will see the screenshot for that movie
--- features/step_definitions/movie.rb
+++ features/step_definitions/movie.rb
@@ -2,6 +2,7 @@ When /^I make a new movie entry and upload a screenshot$/ do
   visit new_movie_path
   fill_in "Title", with: "Matrix, the"
   fill_in "Imdb link", with: "http://www.imdb.com/title/tt0133093/"
+  click_link "add screenshot"
   attach_file("File", "#{Rails.root}/features/support/blank.png")
   click_button "Create Movie"
 end
