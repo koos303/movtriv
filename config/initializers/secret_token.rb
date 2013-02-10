5b4a6f6d81956c5746b63b235def860c07a1620a
Kees Klerk <kees@finalist.com>
2013-02-10 09:26:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movtriv::Application.config.secret_token = '77de27c9257436c78d116b2fbb3d49b10b546c587fc33c355a4ff5b42fbcde276061624d8bcfdf7201e884abcdd8f89fb4e9587f476d2f504f2f4ac1d23c06ee'
