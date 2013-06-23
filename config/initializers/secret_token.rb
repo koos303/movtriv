49af6d92f9141d874315c7a8405d1af9f1ec6c48
Kees Klerk <kees@finalist.com>
2013-06-23 14:14:00 +0200

    Regenerate secret token and key base and check in dummies.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Movtriv::Application.config.secret_key_base = 'soms verzin ik hele goede secrects'
-Movtriv::Application.config.secret_token = '77de27c9257436c78d116b2fbb3d49b10b546c587fc33c355a4ff5b42fbcde276061624d8bcfdf7201e884abcdd8f89fb4e9587f476d2f504f2f4ac1d23c06ee'
+Movtriv::Application.config.secret_token = 'thisisnotthesecrettokenyourelookingfor'
+Movtriv::Application.config.secret_key_base = 'thisisnotthesecretkeybaseyourelookingfor'
